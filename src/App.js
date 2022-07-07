7dfcc7445a0f22607b5428a074201768d1548113
Seth Feldman <namdlefhtes@gmail.com>
2022-07-07 08:40:38 -0400

    working on displaying random cards

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Nav from './Components/Nav';
 import Card from './Components/Card';
 import data from "./data.json";
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+const [charIndex, setCharIndex] = useState();
 
 // start with grid of 12 initially.
 //store card data in JSON or name gen API
@@ -16,12 +19,42 @@ function App() {
 // scoreboard component that tracks current score (state)
 // and best score (state + localstorage)
 
+const randomizer = () => {
+    // use state instead
+   for (let i=0; i < data.characters.length; i++) {
+        // this could just be a random number
+        let randomNum = Math.floor(Math.random() * data.characters.length);
+        // console.log(randomNum);
+        setCharIndex(randomNum);
+
+        if (charIndex) {
+            // console.log(charIndex)
+            // need to take random index and find corresponding character
+            // console.log(data.characters[charIndex]);
+            let randomChar = data.characters[charIndex];
+            console.log(randomChar)
+            // return (
+   
+            // <Card 
+            //      name = {randomChar.name}
+            //      src = {randomChar.src}
+            //      index = {randomChar.index}
+            //      local = {randomChar.local}
+            //      key ={`${randomChar.name}${randomChar.index}`}
+            // />)
+        }
+        
+   }
+
+
+}
 
  return(
     <>
     <Nav />
     <main id="main-container"> 
-    {data.characters.map((card) => 
+    {useEffect( () => randomizer(), [charIndex])}  </main>
+    {/* {data.characters.map((card) => 
         <Card 
             name = {card.name}
             src = {card.src}
@@ -29,9 +62,8 @@ function App() {
             local = {card.local}
             key ={`${card.name}${card.index}`}
         />)
-    }
-    
-    </main>
+    } */}
+  
  </>
  )
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,12 +2,17 @@ import React from "react"
 
 export default function Card (props) {
     //Card grabs image, text, and renders it
-    // console.log(characters)
     return (
 
-        <div id= {`char-${props.index}`}>
+        <div 
+          id= {`char-${props.index}`}
+          className="card-container">
          <h1>{props.name}</h1>
-         <img src={window.location.origin + props.local} alt ={`image of ${props.name}`}/>
+         <img 
+            src={window.location.origin + props.local} 
+            alt ={`image of ${props.name}`}
+            className="card-image"
+          />
          
         </div>
   ) 
