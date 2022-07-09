015dba6fbe8f34646ef5b3cc7ec6564a23c2b2a2
Seth Feldman <namdlefhtes@gmail.com>
2022-07-09 11:32:34 -0400

    images display in random order

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
 
-const [charIndex, setCharIndex] = useState();
+const [charIndex, setCharIndex] = useState([0,1,2,3,4,5,6]);
+const [charArr, setCharArr] = useState([]);
 
 // start with grid of 12 initially.
 //store card data in JSON or name gen API
@@ -20,32 +21,24 @@ const [charIndex, setCharIndex] = useState();
 // and best score (state + localstorage)
 
 const randomizer = () => {
-    // use state instead
-   for (let i=0; i < data.characters.length; i++) {
-        // this could just be a random number
-        let randomNum = Math.floor(Math.random() * data.characters.length);
-        // console.log(randomNum);
-        setCharIndex(randomNum);
-
-        if (charIndex) {
-            // console.log(charIndex)
-            // need to take random index and find corresponding character
-            // console.log(data.characters[charIndex]);
-            let randomChar = data.characters[charIndex];
-            console.log(randomChar)
-            // return (
-   
-            // <Card 
-            //      name = {randomChar.name}
-            //      src = {randomChar.src}
-            //      index = {randomChar.index}
-            //      local = {randomChar.local}
-            //      key ={`${randomChar.name}${randomChar.index}`}
-            // />)
-        }
-        
-   }
-
+    // loop should use charIndex array length
+    // store random nums in a set of unique values within charIndex state.
+        // charIndex has vals 0-6 in arr
+        // rearrange values
+        let temp = charIndex;
+        temp.sort( () => Math.random() - 0.5);
+        setCharIndex(temp);   
+        console.log(charIndex)
+    //when random nums in array, apply sequentially with loop to character index
+    for (let i = 0; i < data.characters.length; i++) {
+        let randomChar = data.characters[charIndex[i]];
+        console.log(randomChar);
+        // this creates double the length I want
+        setCharArr(prevState => (
+            [...prevState,
+            randomChar]
+        ));  
+    }
 
 }
 
@@ -53,18 +46,25 @@ const randomizer = () => {
     <>
     <Nav />
     <main id="main-container"> 
-    {useEffect( () => randomizer(), [charIndex])}  </main>
-    {/* {data.characters.map((card) => 
-        <Card 
-            name = {card.name}
-            src = {card.src}
-            index = {card.index}
-            local = {card.local}
-            key ={`${card.name}${card.index}`}
-        />)
-    } */}
-  
- </>
+    {useEffect( () => randomizer(), [])}  </main>
+
+    {charIndex ? 
+    charArr.map((char)=> {
+        return (
+            <Card 
+            name = {char.name}
+            src = {char.src}
+            index = {char.index}
+            local = {char.local}
+            key ={`${char.name}${char.index}`}
+        />
+        )
+    })
+    :
+    null
+    }
+
+    </>
  )
 }
 
