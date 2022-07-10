9a29f1117a03d9157ad124558abee7c73f6e04f7
Seth Feldman <namdlefhtes@gmail.com>
2022-07-10 11:05:52 -0400

    score keeping and reset on duplicate

--- src/App.js
+++ src/App.js
@@ -2,37 +2,32 @@ import Nav from './Components/Nav';
 import Card from './Components/Card';
 import data from "./data.json";
 import React, { useState, useEffect } from 'react';
+import { click } from '@testing-library/user-event/dist/click';
 
 function App() {
 
 const [charIndex, setCharIndex] = useState([0,1,2,3,4,5,6]);
 const [charArr, setCharArr] = useState([]);
+const [clickedChars, setClickedChars] = useState([]);
 
 // start with grid of 12 initially.
 //store card data in JSON or name gen API
 // images stored in dir or API.
 
-
-
 // create card component to gen each card
-// use state to track whether cards have been clicked
 // scoreboard component that tracks current score (state)
 // and best score (state + localstorage)
 
-
-
 // map cards in random order
 const randomizer = () => {
-    // loop should use charIndex array length
-    // store random nums in a set of unique values within charIndex state.
         let temp = charIndex;
         temp.sort( () => Math.random() - 0.5);
         setCharIndex(temp);   
-        console.log(charIndex)
+        // console.log(charIndex)
     //when random nums in array, apply sequentially with loop to character index
     for (let i = 0; i < data.characters.length; i++) {
         let randomChar = data.characters[charIndex[i]];
-        console.log(randomChar);
+        // console.log(randomChar);
         // this creates double the length I want
         setCharArr(prevState => (
             [...prevState,
@@ -41,15 +36,30 @@ const randomizer = () => {
     }
 };
 
+// use state to track whether cards have been clicked
 const handleClick = (index) => {
-    console.log(index)
-    setCharArr([]);
+    // update clickedChars array to track clicked chars.
+    setClickedChars(prevState=>
+        [...prevState,
+          index  
+        ])
+    // take length of clickedChars and create score
+     // check if clickedArr contains duplicates 
+    if (clickedChars.includes(index)) {
+        // reset clickedChars + score
+        setClickedChars([]);
+        console.log("Oops, you already clicked that!");
+    }
+    // reset charArr state to run randomizer        
+    setCharArr([]); 
     randomizer();
 };
 
  return(
     <>
-        {/* <Nav /> */}
+        <Nav
+            score = {clickedChars.length}
+        />
         <main id="main-container"> 
             {useEffect( () => randomizer(), [])}  
 
