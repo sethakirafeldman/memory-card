dea76edce5e1b9356ac16388b7c6aff6344e4d70
Seth Feldman <namdlefhtes@gmail.com>
2022-07-10 11:05:32 -0400

    scoreboard

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
-export default function Nav () {
-    return(<nav>Nav</nav>)
+export default function Nav (props) {
+    return(
+
+    <nav>Score: {props.score}</nav>
+    
+    )
 }
