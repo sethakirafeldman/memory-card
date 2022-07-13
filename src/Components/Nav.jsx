371145d8b49a16a314fd197bef899316b7e7b246
Seth Feldman <namdlefhtes@gmail.com>
2022-07-13 08:42:16 -0400

    high score stores

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 export default function Nav (props) {
-    return(
+
+    const [highScore, setHighScore] = useState(0);
+
+    const updateScore = () => {
+        // sets tempScore arr to highScore val
+        let tempScore = [highScore]; 
+        tempScore.push(props.score);
+        //sorts scores from lowest to highest
+        tempScore.sort();
+        // get highest score in array.
+        let highest = tempScore[tempScore.length -1];
+        setHighScore(highest);
+    }
+
+    useEffect(()=>{
+        if (props.gameStatus) {
+            updateScore();
+        }
+    },[props.score]);
+    
+    return (
 
     <nav>
         <h1>Pixar Memory Game</h1>
@@ -10,7 +30,9 @@ export default function Nav (props) {
             :
             <h1 className='game-over'>Game over!</h1>
         }
-        <h1>High Score: </h1>
+    
+        <h1>High Score: {highScore} </h1>
+ 
         </nav>
     )
 }
